1d0d3507dcf563074b79a6c06a5faa65746e120d
梅 <plumcat@meide-MacBook-Pro.local>
2019-12-16 16:47:38 +0800

    revised

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ from args import get_args
 if __name__ == '__main__':
     args = get_args()
     crawler = Crawler()
-    content = crawler.crawl(2019-11-10,2019-12-10 )
-#    content = crawler.crawl(args.start_date, args.end_date)
+    content = crawler.crawl(args.start_date, args.end_date)
     # TODO: write content to file according to spec
